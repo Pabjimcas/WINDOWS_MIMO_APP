a838d141e4de9941d6bd2a3abb960fc672b53a2a
Pablo <pabjimcas@gmail.com>
2016-06-16 18:51:24 +0200

    Create NavigationServices

---
+++ WINDOWS_MIMO_APP_2/Services/NavigationService/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿namespace WINDOWS_MIMO_APP_2.Services.NavigationService
+{
+    using Windows.UI.Xaml.Controls;
+    internal interface INavigationService
+    {
+        Frame AppFrame { set; }
+        void GoBack();
+    }
+}
---
+++ WINDOWS_MIMO_APP_2/Services/NavigationService/NavigationService.cs
@@ -0,0 +1,19 @@
+﻿namespace WINDOWS_MIMO_APP_2.Services.NavigationService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Windows.UI.Xaml.Controls;
+    public class NavigationService : INavigationService
+    {
+        public Frame AppFrame { private get; set; }
+        public void GoBack()
+        {
+            if (AppFrame != null && AppFrame.CanGoBack)
+                AppFrame.GoBack();
+        }
+
+    }
+}
