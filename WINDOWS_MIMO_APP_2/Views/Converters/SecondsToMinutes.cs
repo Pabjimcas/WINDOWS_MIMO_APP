9ec1c9cd51edc034ffa06b7df77dbf6ae82672ab
Pablo <pabjimcas@gmail.com>
2016-06-23 18:29:20 +0200

    Converters

---
+++ WINDOWS_MIMO_APP_2/Views/Converters/SecondsToMinutes.cs
@@ -0,0 +1,27 @@
+﻿namespace WINDOWS_MIMO_APP_2.Converters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Windows.UI.Xaml.Data;
+    class SecondsToMinutes : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value != null)
+            {
+                var minutes = System.Convert.ToInt32(value.ToString()) / 60;
+                return minutes+" minutos";
+            }
+
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
