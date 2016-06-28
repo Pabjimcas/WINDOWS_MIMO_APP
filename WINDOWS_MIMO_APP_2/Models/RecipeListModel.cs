d56c73fc1498699c12d26adf6292c2aa069aca04
mikel balduciel <mibaldi2@gmail.com>
2016-06-29 00:40:10 +0200

    clean

--- WINDOWS_MIMO_APP_2/Models/RecipeListModel.cs
+++ WINDOWS_MIMO_APP_2/Models/RecipeListModel.cs
@@ -2,11 +2,7 @@
 
 namespace WINDOWS_MIMO_APP_2.Models
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    
     public class RecipeList
     {
         public int id { get; set; }
